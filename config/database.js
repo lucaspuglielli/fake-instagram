d55ffe21d72fbd3a5ac612f190f4a804fa678a6b
lucaspuglielli <lucaspuglielli@gmail.com>
2020-04-29 16:46:06 -0300

    subindo a base do projeto

---
+++ config/database.js
@@ -0,0 +1,10 @@
+const config = {
+    username: 'root',
+    password: '',
+    database: 'fake-instagram',
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306,
+}
+
+module.exports = config
